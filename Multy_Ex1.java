a0514a91e35989a0357031fb3052c61e669789d3
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-08-16 23:16:37 +0530

    Add files via upload

---
+++ Multy_Ex1.java
@@ -0,0 +1,16 @@
+public class Multy_Ex1 {
+
+	public static void main(String[] args) {
+        int a[][] = {{10,20}, {30,40} };
+        for(int i = 0; i<2; i++)
+        {
+        	for (int j =0; j<2; j++)
+        	{
+        		System.out.print(a[i][j] +"\t");
+        	}
+        	System.out.println();
+        }
+		
+	}
+
+}
